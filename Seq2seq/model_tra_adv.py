bf83b513307f31261d52cfdcb5f83647f9d0b336
changwookjun <changwookjun@gmail.com>
2019-07-11 17:37:47 +0900

    modify model_tra_adv.py

--- Seq2seq/model_tra_adv.py
+++ Seq2seq/model_tra_adv.py
@@ -167,16 +167,21 @@ def Model(features, labels, mode, params):
 
     for i in range(loop_count):
         with tf.variable_scope('decoder', reuse=tf.AUTO_REUSE):
+            print("decoder i : ", i)
             if i > 0:
                 output = tf.concat([tf.ones((output.shape[0], 1), dtype=tf.int64), predict[:, :-1]], axis=-1)
+                print("i > 0 output: ", output)
             else:
                 output = features['output'] # ?, 25
+                print("output: ", output)
 
             y_embedded_matrix = embedding(output) + position_encode # ?, 25, 128
             decoder_outputs = decoder_layers(y_embedded_matrix, encoder_outputs) # ?, 25, 128
-
+            print("decoder_outputs: ", decoder_outputs)
             logits = logit_layer(decoder_outputs) # ?, 25, 12657
+            print("logits: ", logits)
             predict = tf.argmax(logits, 2) # ?, 25
+            print("predict: ", predict)
 
     if PREDICT:
         predictions = {
